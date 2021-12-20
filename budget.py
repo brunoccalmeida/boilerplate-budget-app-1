0c1144757adcfe62fc7f7deac90588d13301b6fd
brunoccalmeida <Ba177601!@#$!@>
2021-12-20 19:53:34 -0300

    Trabalhado em create_spend_chart

--- budget.py
+++ budget.py
@@ -1,18 +1,17 @@
 class Category:
 
-
-
     def __init__(self, name, funds=0):
         self.funds = funds
         self.name = name
         self.ledger = []
+
     def __str__(self):
         fh = open("list_of_elements.txt", "w+")
         for element in self.ledger:
             if len(element["description"]) > 23:
                 element["description"] = element["description"][0:23]
-            fh.write(f'{element["description"]:<23}{element["amount"]:>7}\n')
-        fh.write('\n')
+            fh.write(f'{element["description"]:<23}{element["amount"]:>7.2f}\n')
+        fh.write(f'Total: {self.funds}')
         fh.close()
         with open("list_of_elements.txt", "r+") as fh:
             return f'{self.name:*^30}\n{fh.read()}'
@@ -45,15 +44,45 @@ class Category:
 
 
 def create_spend_chart(categories):
-    pass
+    list_of_categories = categories
+    number_of_categories = len(list_of_categories)
+    dict_of_variables = {}
+    for n in range(1, number_of_categories+1):
+        dict_of_variables[f'cat{n}'] = 0
+    for category in list_of_categories:
+        print(category.__repr__())
+
+
+
+
+    #         if cat.name == 'Food' and element['amount'] < 0:
+    #             food_total_spent += element['amount']
+    #         if cat.name == 'Clothing' and element['amount'] < 0:
+    #             clothing_total_spent += element['amount']
+    #         if cat.name == 'Entertainment' and element['amount'] < 0:
+    #             entertainment_total_spent += element['amount']
+    # total = food_total_spent + clothing_total_spent + entertainment_total_spent
+    # food_percent = (food_total_spent / total) * 100
+    # clothing_percent = (clothing_total_spent / total) * 100
+    # entertainment_percent = (entertainment_total_spent / total) * 100
+    # print(f'Food: {food_percent:.0f} Clothing: {clothing_percent:.0f} Entertainment: {entertainment_percent:.0f}')
 
 
 if __name__ == '__main__':
-    food = Category('Food')
-    clothing = Category('Clothing')
-    food.deposit(1000, 'deposito inicial')
-    food.deposit(900, 'deposito')
-    food.withdraw(300, 'comida do mes de fevereiro de 2004 no natal')
-    food.transfer(100, clothing)
-    print(food)
-
+    food = Category('Food', funds=1000)
+    clothing = Category('Clothing', funds=1000)
+    entertainment = Category('Entertainment', funds=1000)
+    food.withdraw(20, 'groceries')
+    food.withdraw(50, 'icecream')
+    food.withdraw(100, 'chocolate')
+    food.deposit(10)
+    clothing.withdraw(50, 'T-shirt')
+    clothing.withdraw(80, 'sockes and underware')
+    clothing.withdraw(30, 'gloves')
+    clothing.withdraw(90, 'trousers')
+    clothing.deposit(80)
+    entertainment.withdraw(100, 'movies')
+    entertainment.withdraw(90, 'pc game')
+    entertainment.deposit(50)
+    cat_list = [food, clothing, entertainment]
+    create_spend_chart(cat_list)
--- tests/testes_unitarios.py
+++ tests/testes_unitarios.py
@@ -1,47 +1,47 @@
 import budget
 
 
-def initialize_food():
-    food = budget.Category("food")
-    food.deposit(1000, "deposito inicial")
-    return food
+def initialize(category):
+    category_name = budget.Category(f"{category}")
+    category_name.deposit(1000, "deposito inicial")
+    return category_name
 
 
-def closing_food(food):
-    food.funds = 0
-    food.ledger.clear()
+def close(name):
+    name.funds = 0
+    name.ledger.clear()
 
 
 def test_ledger():
-    food = initialize_food()
+    food = initialize('Food')
     clothing = budget.Category('clothing')
     assert food.ledger[-1] == {"amount": 1000, "description": "deposito inicial"}
     food.withdraw(250, "almoço de ontem")
     assert food.ledger[-1] == {"amount": -250, "description": "almoço de ontem"}
     food.transfer(300, clothing)
     assert food.ledger[-1] == {"amount": -300, "description": "Transfer to clothing"}
-    assert clothing.ledger[-1] == {"amount": 300, "description": "Transfer from food"}
-    closing_food(food)
+    assert clothing.ledger[-1] == {"amount": 300, "description": "Transfer from Food"}
+    close(food)
 
 
 def test_deposit():
-    food = initialize_food()
+    food = initialize('Food')
     assert food.funds == 1000
     food.deposit(2000, "salario do mes")
     assert food.funds == 3000
-    closing_food(food)
+    close(food)
 
 
 def test_withdraw():
-    food = initialize_food()
+    food = initialize('Food')
     food.withdraw(250, "almoço de ontem")
     assert food.funds == 750
     assert food.withdraw(1500) is False
-    closing_food(food)
+    close(food)
 
 
 def test_get_balance():
-    food = initialize_food()
+    food = initialize('Food')
     assert food.get_balance() == 1000
     food.withdraw(800)
     assert food.get_balance() == 200
@@ -49,13 +49,12 @@ def test_get_balance():
     assert food.get_balance() == 200
     food.deposit(300)
     assert food.get_balance() == 500
-    closing_food(food)
+    close(food)
 
 
 def test_transfer():
-    food = initialize_food()
-    clothing = budget.Category("clothing")
-    clothing.deposit(1000, "deposito inicial")
+    food = initialize('Food')
+    clothing = initialize("Clothing")
     food.transfer(300, clothing)
     assert food.funds == 700
     assert clothing.funds == 1300
@@ -63,18 +62,42 @@ def test_transfer():
     assert food.funds == 700
     assert clothing.funds == 1300
     clothing.funds = 0
-    closing_food(food)
+    close(food)
+    close(clothing)
 
 
 def test_check_funds():
-    food = initialize_food()
+    food = initialize('Food')
     assert food.check_funds(1200) is False
     assert food.check_funds(1000) is True
     assert food.check_funds(1001) is False
     assert food.check_funds(999) is True
     assert food.check_funds(1) is True
-    closing_food(food)
+    close(food)
 
 
 def test_to_string():
-    food = initialize_food()
+    food = initialize('Food')
+    food.withdraw(10.15, "groceries")
+    food.withdraw(15.89, "restaurant and more food for dessert")
+    assert str(food) == f"*************Food*************\ndeposito inicial       1000.00\n" \
+                        f"groceries               -10.15\nrestaurant and more foo -15.89\nTotal: 973.96"
+
+
+def test_create_spend_chart():
+    food = initialize('Food')
+    clothing = initialize('Clothing')
+    entertainment = initialize('entertainment')
+    food.withdraw(20, 'groceries')
+    food.withdraw(50, 'icecream')
+    food.withdraw(100, 'chocolate')
+    food.deposit(10)
+    clothing.withdraw(50, 'T-shirt')
+    clothing.withdraw(80, 'sockes and underware')
+    clothing.withdraw(30, 'gloves')
+    clothing.withdraw(90, 'trousers')
+    clothing.deposit(80)
+    entertainment.withdraw(100, 'movies')
+    entertainment.withdraw(90, 'pc game')
+    entertainment.deposit(50)
+
